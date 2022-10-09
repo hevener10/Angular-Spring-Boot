beb23e353840167a8c99c070e0d05823aa3b6ed0
Hevener Ancelmo Pereira <hevener2@hotmail.com>
2022-10-09 11:57:07 -0300

    Criando multiplos perfils

---
+++ src/main/java/com/github/hevener10/repository/ClientesRepository.java
@@ -0,0 +1,11 @@
+package com.github.hevener10.repository;
+
+import com.github.hevener10.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+    public void persistir(Cliente cliente) {
+        //Acessa a base e salva o cliente
+    }
+}
